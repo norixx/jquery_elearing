401cc13d4d2b41cc7a7b7213fece6181218ef9a7
Nori Tsutae <nori2tae@gmail.com>
2022-09-12 11:48:58 +0900

    init

---
+++ js/jsonajax.js
@@ -0,0 +1,17 @@
+const cars = [
+  {
+    "name": "カローラ",
+    "type": "セダン",
+    "brand": "TOYOTA"
+  },
+  {
+    "name": "フィット",
+    "type": "コンパクト",
+    "brand": "HONDA"
+  },
+  {
+    "name": "ハイゼット",
+    "type": "軽貨物車",
+    "brand": "ダイハツ"
+  }
+];
