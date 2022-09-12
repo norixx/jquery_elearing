e9ed7ceae02272da5a2d399167d11d2b2ed54346
Nori Tsutae <nori2tae@gmail.com>
2022-09-12 15:38:20 +0900

    mini update

--- js/jsonajax.js
+++ js/jsonajax.js
@@ -1,17 +1,39 @@
-const cars = [
-  {
-    "name": "カローラ",
-    "type": "セダン",
-    "brand": "TOYOTA"
-  },
-  {
-    "name": "フィット",
-    "type": "コンパクト",
-    "brand": "HONDA"
-  },
-  {
-    "name": "ハイゼット",
-    "type": "軽貨物車",
-    "brand": "ダイハツ"
-  }
-];
+// const cars = [
+//   {
+//     "name": "カローラ",
+//     "type": "セダン",
+//     "brand": "TOYOTA"
+//   },
+//   {
+//     "name": "フィット",
+//     "type": "コンパクト",
+//     "brand": "HONDA"
+//   },
+//   {
+//     "name": "ハイゼット",
+//     "type": "軽貨物車",
+//     "brand": "ダイハツ"
+//   }
+// ];
+
+const btn = document.querySelector('#btn');
+const info = document.querySelector('#car-info');
+const request = new XMLHttpRequest();
+
+btn.addEventListener('click', () => {
+
+  request.open('GET', 'https://norixx.github.io/jquery_elearing/cars.json');
+  request.onload = () => {
+    const data = JSON.parse(request.responseText);
+    outputHtml(data);
+  };
+  request.send();
+});
+
+function outputHtml(data) {
+  let htmlStr = '';
+  data.forEach( car => {
+    htmlStr = `<p>${car.name}は${car.brand}の${car.type}車です。</p>`;
+  });
+  info.insertAdjacentHTML('beforeend', htmlStr);
+}
